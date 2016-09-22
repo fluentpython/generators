81c229e3dcc01f2c4bfeb72cd0ed07c8f2d47b38
Luciano Ramalho <luciano@ramalho.org>
2016-09-22 20:51:36 -0300

    refactored using  flag

--- primes/primes.py
+++ primes/primes.py
@@ -3,30 +3,28 @@
 from array import array
 
 TYPECODE = 'Q'  # 64-bit unsigned integer
-
+THRESHOLD = 2**64
 
 def primes():
-    """yield primes; stop at first prime after 2**64"""
+    """Prime number generator"""
+
     yield 2
     seen = array(TYPECODE)
     candidate = 3
 
     while True:
-        factor = None
+        composite = False
 
         for n in seen:
             if candidate % n == 0:
-                factor = n
+                composite = True
                 break
             if n * n > candidate:
                 break
 
-        if factor is None:
+        if not composite:
             yield candidate
-            try:
-                seen.append(candidate)
-            except OverflowError:
-                return
+            seen.append(candidate)  # OverflowError if candidate >= 2**64
 
         candidate += 2
 
@@ -38,7 +36,10 @@ if __name__ == '__main__':
         num_primes = int(sys.argv[-1])
     except ValueError:
         print('Usage: {} <how_many_primes>'.format(sys.argv[0]))
+        print('\t' + primes.__doc__)
         sys.exit(-1)
 
     for _, p in zip(range(num_primes), primes()):
-        print('{:,}'.format(p))
+        print('{:,}'.format(p), end=' ')
+
+    print()
